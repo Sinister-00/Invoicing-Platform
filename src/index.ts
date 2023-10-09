473a999f59107d4aa7780ac9a76899f919c359bd
Sinister-00 <sswapnil_be20@thapar.edu>
2023-10-09 11:01:23 +0530

    Project setup
    Signed-off-by: Sinister-00 <sswapnil_be20@thapar.edu>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, {Express, Request, Response} from 'express';
+import cors from "cors";
+import helmet from "helmet";
+import "dotenv/config";
 
 const app: Express = express();
 const port = 3000;
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is Express + TypeScript');
