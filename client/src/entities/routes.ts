79f2a02455e6dd81c46ee6e6c3fd8629eb03f3bc
Sinister-00 <sswapnil_be20@thapar.edu>
2023-10-10 14:19:00 +0530

    feat: Folder setup

---
+++ client/src/entities/routes.ts
@@ -0,0 +1,13 @@
+export enum ROUTES {
+  HOME = '/',
+  ABOUT = '/about',
+  PRODUCTS = '/products',
+  CONTACT = '/contact',
+  PRODUCT = '/single-product/:id',
+  CART = '/cart',
+  SIGNIN = '/signin',
+  LOGIN = '/login',
+  BILL = '/bill',
+  ORDER_PLACED = '/order-placed',
+  ORDER_HISTORY = '/order-history',
+}
---
+++ client/src/views/order-history/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrderHistoryPage = () => {
+  return <></>;
+};
+
+export default OrderHistoryPage;
